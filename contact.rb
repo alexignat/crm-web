eea2df3078b88108a068ec4abb2bec0b320d4f42
alexignat <d.alexandru.ignat@gmail.com>
2015-06-16 15:08:32 -0400

    Added rolodex file

--- contact.rb
+++ contact.rb
@@ -2,13 +2,14 @@ class Contact
   attr_accessor :first_name, :last_name, :email, :note
   attr_reader :id
 
-  @@id = 1
+  # @@id = 1 This assingment has id stored in the rolodex
 
   def initialize(first_name, last_name, email, note)
     @id = @@id
-    @@id += 1
+    # @@id += 1 This assingment has id stored in the rolodex
     @first_name = first_name.capitalize
     @last_name = last_name.capitalize
     @email = email
     @note = note
   end
+end
