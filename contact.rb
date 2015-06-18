71b7b1c0747656c5a3b5d041491c3c6fe9256205
alexignat <d.alexandru.ignat@gmail.com>
2015-06-18 15:20:10 -0400

    Added capitalize on variables

--- contact.rb
+++ contact.rb
@@ -2,8 +2,8 @@ class Contact
   attr_accessor :id, :first_name, :last_name, :email, :note
 
   def initialize(first_name, last_name, email, note)
-    @first_name = first_name
-    @last_name = last_name
+    @first_name = first_name.capitalize
+    @last_name = last_name.capitalize
     @email = email
     @note = note
   end
